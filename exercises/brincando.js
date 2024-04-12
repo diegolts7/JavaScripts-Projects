05d1e8beb18fa1c8e77ef790be23c9bec92b9ab6
Diego Sousa <diego7lts7@gmail.com>
2024-04-12 20:09:12 -0300

    treinando em js

---
+++ exercises/brincando.js
@@ -0,0 +1,11 @@
+const items = [
+    'Oranges',
+    'Oregano',
+    'Mozzarella cheese',
+    'Milk',
+    'Detergent powder'
+ ];
+
+ document.write("<h2>Lista de compras</h2>");
+ document.write(`<ol><li>${items[0]}</li><br/><li>${items[1]}</li><br/><li>${items[2]}</li><br/><li>${items[3]}</li><br/><li>${items[4]}</li>
+ </ol>`)
